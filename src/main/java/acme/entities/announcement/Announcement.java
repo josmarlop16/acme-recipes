ccad723614bbe571bd601038f35b03b553f5d2bd
jvegax <toledo.utrera@hotmail.com>
2022-03-02 19:31:49 +0100

    update announcement entity

--- src/main/java/acme/entities/announcement/Announcement.java
+++ src/main/java/acme/entities/announcement/Announcement.java
@@ -1,18 +1,20 @@
 package acme.entities.announcement;
 
-import acme.framework.entities.AbstractEntity;
-
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -25,6 +27,7 @@ public class Announcement extends AbstractEntity {
 
 	// Attributes -------------------------------------------------------------
 	@Past
+	@Temporal(TemporalType.TIMESTAMP)
 	protected Date creation;
 	
 	@NotBlank
@@ -37,6 +40,7 @@ public class Announcement extends AbstractEntity {
 	
 	protected Boolean critical;
 	
+	@OneToOne(optional = true)
 	@URL
 	protected String optionalLink;
 
