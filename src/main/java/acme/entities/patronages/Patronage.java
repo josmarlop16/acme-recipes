f9ddf499b6317da3d0d4d39f924cd895fa5f46ef
LuisUsrDev <luirodgar3@alum.us.es>
2022-03-03 11:19:47 +0100

    Task-028 : done

---
+++ src/main/java/acme/entities/patronages/Patronage.java
@@ -0,0 +1,73 @@
+package acme.entities.patronages;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import acme.roles.Inventor;
+import acme.roles.Patron;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Patronage extends AbstractEntity{
+	
+	// Serialisation identifier -----------------------------------------------
+	protected static final long serialVersionUID = 1L;
+	
+	// Attributes --------------------------------------------------------------
+	@Enumerated
+	protected PatronageStatus status;
+	
+	@Column(unique = true)
+	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
+	protected String code;
+	
+	@NotBlank
+	@Length(min = 0, max = 256)
+	protected String stuff;
+	
+	@Positive
+	protected Integer budget;	
+	
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date periodOfTime;
+	
+	@URL
+	protected String optionalLink;
+	
+	// Derived attributes -----------------------------------------------------
+	
+	// Relationships ----------------------------------------------------------
+	
+	@NotNull
+	@Valid
+	@OneToOne(optional = false)
+	protected Patron patron;
+	
+	@NotNull
+	@Valid
+	@OneToOne(optional = false)
+	protected Inventor inventor;
+	
+	
+
+}
+
