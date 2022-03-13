2815b48d893ea6d49913b9d0ef837a1a765f0b0f
jvegax <toledo.utrera@hotmail.com>
2022-03-13 15:53:00 +0100

    working on patron dashboard

--- src/main/java/acme/features/patron/dashboard/PatronDashboardRepository.java
+++ src/main/java/acme/features/patron/dashboard/PatronDashboardRepository.java
@@ -12,6 +12,7 @@
 
 package acme.features.patron.dashboard;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.framework.repositories.AbstractRepository;
@@ -19,51 +20,37 @@ import acme.framework.repositories.AbstractRepository;
 @Repository
 public interface PatronDashboardRepository extends AbstractRepository {
 
+	@Query("select count(t) from Patron t")
 	Integer totalNumberOfProposedPatronages();
 
+	@Query("select count(t) from Patron t")
 	Integer totalNumberOfAcceptedPatronages();
 
+	@Query("select count(t) from Patron t")
 	Integer totalNumberOfDeniedPatronages();
 
+	@Query("select count(t) from Patron t")
 	Double averageNumberOfPatronages();
 
+	@Query("select count(t) from Patron t")
 	Long deviationOfPatronages();
 
+	@Query("select count(t) from Patron t")
 	Double minimumBudgetOfProposedPatronages();
 
+	@Query("select count(t) from Patron t")
 	Double minimumBudgetOfAcceptedPatronages();
 
+	@Query("select count(t) from Patron t")
 	Double minimumBudgetOfDeniedPatronages();
 
+	@Query("select count(t) from Patron t")
 	Double maximumBudgetOfProposedPatronages();
 
+	@Query("select count(t) from Patron t")
 	Double maximumBudgetOfAcceptedPatronages();
 
+	@Query("select count(t) from Patron t")
 	Double maximumBudgetOfDeniedPatronages();
-	
-	
-	
-	
-	
-	
-	
-
-//	@Query("select avg(select count(j) from Job j where j.employer.id = e.id) from Employer e")
-//	Double averageNumberOfJobsPerEmployer();
-//
-//	@Query("select avg(select count(a) from Application a where a.worker.id = w.id) from Worker w")
-//	Double averageNumberOfApplicationsPerWorker();
-//
-//	@Query("select avg(select count(a) from Application a where exists(select j from Job j where j.employer.id = e.id and a.job.id = j.id)) from Employer e")
-//	Double averageNumberOfApplicationsPerEmployer();
-//
-//	@Query("select 1.0 * count(a) / (select count(b) from Application b) from Application a where a.status = acme.entities.jobs.ApplicationStatus.PENDING")
-//	Double ratioOfPendingApplications();
-//
-//	@Query("select 1.0 * count(a) / (select count(b) from Application b) from Application a where a.status = acme.entities.jobs.ApplicationStatus.ACCEPTED")
-//	Double ratioOfAcceptedApplications();
-//
-//	@Query("select 1.0 * count(a) / (select count(b) from Application b) from Application a where a.status = acme.entities.jobs.ApplicationStatus.REJECTED")
-//	Double ratioOfRejectedApplications();
-
+ 
 }
