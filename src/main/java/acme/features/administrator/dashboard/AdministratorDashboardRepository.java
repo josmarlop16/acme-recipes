fa1784efb60cc39c7ca900da23b29a68ca27d579
jose caceres gomez <joscacgom@alum.us.es>
2022-03-20 14:43:32 +0100

    Task-031:Fixed 4

--- src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
+++ src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
@@ -1,12 +1,10 @@
 package acme.features.administrator.dashboard;
 
-import java.util.Map;
+import java.util.List;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import acme.entities.patronages.PatronageStatus;
 import acme.framework.repositories.AbstractRepository;
 
 @Repository
@@ -22,64 +20,107 @@ public interface AdministratorDashboardRepository extends AbstractRepository {
 	int totalNumberOfPatronages();
 	
 	@Query("select p.status, count(p) from Patronage p group by p.status")
-	Map<PatronageStatus,Integer> totalNumberOfPatronagesGroupedByPatronageStatus();
+	List<Object[]> totalNumberOfPatronagesGroupedByPatronageStatus();
+//	@Query("select p.status, count(p) from Patronage p group by p.status")
+//	Map<PatronageStatus,Integer> totalNumberOfPatronagesGroupedByPatronageStatus();
 // 	@Query("select count(p) from Patronages p group by p.status")
 // 	int totalNumberOfPatronages();
 
 //	Average 
 
 	@Query("select c.technology,c.retailPrice.currency, avg(c.retailPrice.amount) from Item c  where c.type=1 group by c.technology,c.retailPrice.currency")
-	Map<Pair<String,String>,Double> averageRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+	List<Object[]> averageRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+//	@Query("select c.technology,c.retailPrice.currency, avg(c.retailPrice.amount) from Item c  where c.type=1 group by c.technology,c.retailPrice.currency")
+//	Map<Pair<String,String>,Double> averageRetailPriceOfComponentGroupedByTechnologyAndCurrency();
 //  @Query("select avg(c.retailPrice.amount) from Item c where c.type=1 group by c.technology, c.retailPrice.currency")
 //	Double averageRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+	
+	
 	@Query("select t.retailPrice.currency, avg(t.retailPrice.amount) from Item t where t.type=0 group by t.retailPrice.currency")
-	Map<String,Double> averageRetailPriceOfToolGroupedByCurrency();
+	List<Object[]> averageRetailPriceOfToolGroupedByCurrency();
+//	@Query("select t.retailPrice.currency, avg(t.retailPrice.amount) from Item t where t.type=0 group by t.retailPrice.currency")
+//	Map<String,Double> averageRetailPriceOfToolGroupedByCurrency();
 //  @Query("select avg(t.retailPrice.amount) from Item t where t.type=0 group by t.retailPrice.currency")
 //	Double averageRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+	
 	@Query("select p.status ,avg(p.budget) from Patronage p group by p.status")
-	Map<PatronageStatus,Double> averageBudgetOfPatronagesGroupedByPatronageStatus();
+	List<Object[]> averageBudgetOfPatronagesGroupedByPatronageStatus();
+//	@Query("select p.status ,avg(p.budget) from Patronage p group by p.status")
+//	Map<PatronageStatus,Double> averageBudgetOfPatronagesGroupedByPatronageStatus();
 //  @Query("select avg(p.budget) from Patronage p group by p.status")
 //	Double averageBudgetOfPatronagesGroupedByPatronageStatus();
 	
 //	Deviation
-
+	
 	@Query("select c.technology,c.retailPrice.currency, stddev(c.retailPrice.amount) from Item c where c.type=1 group by c.technology,c.retailPrice.currency")
-	Map<Pair<String,String>,Double> deviationRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+	List<Object[]> deviationRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+//	@Query("select c.technology,c.retailPrice.currency, stddev(c.retailPrice.amount) from Item c where c.type=1 group by c.technology,c.retailPrice.currency")
+//	Map<Pair<String,String>,Double> deviationRetailPriceOfComponentGroupedByTechnologyAndCurrency();
 //  @Query("select stddev(c.retailPrice.amount) from Item c where c.type=1 group by c.technology, c.retailPrice.currency")
 //	Double deviationRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+	
 	@Query("select t.retailPrice.currency, stddev(t.retailPrice.amount) from Item t where t.type=0 group by t.retailPrice.currency")
-	Map<String,Double> deviationRetailPriceOfToolGroupedByCurrency();
+	List<Object[]> deviationRetailPriceOfToolGroupedByCurrency();
+//	@Query("select t.retailPrice.currency, stddev(t.retailPrice.amount) from Item t where t.type=0 group by t.retailPrice.currency")
+//	Map<String,Double> deviationRetailPriceOfToolGroupedByCurrency();
 //  @Query("select stddev(t.retailPrice.amount) from Item t where t.type=0 group by t.retailPrice.currency")
 //	Double deviationRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+	
 	@Query("select p.status ,stddev(p.budget) from Patronage p group by p.status")
-	Map<PatronageStatus,Double> deviationBudgetOfPatronagesGroupedByPatronageStatus();
+	List<Object[]> deviationBudgetOfPatronagesGroupedByPatronageStatus();
+//	@Query("select p.status ,stddev(p.budget) from Patronage p group by p.status")
+//	Map<PatronageStatus,Double> deviationBudgetOfPatronagesGroupedByPatronageStatus();
 //  @Query("select stddev(p.budget) from Patronage p group by p.status")
 //	Double deviationBudgetOfPatronagesGroupedByPatronageStatus();
+	
 //	Minimum 
+	
 	@Query("select c.technology,c.retailPrice.currency, min(c.retailPrice.amount) from Item c where c.type=1 group by c.technology,c.retailPrice.currency")
-	Map<Pair<String,String>,Double> minimumRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+	List<Object[]> minimumRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+//	@Query("select c.technology,c.retailPrice.currency, min(c.retailPrice.amount) from Item c where c.type=1 group by c.technology,c.retailPrice.currency")
+//	Map<Pair<String,String>,Double> minimumRetailPriceOfComponentGroupedByTechnologyAndCurrency();
 //  @Query("select min(c.retailPrice.amount) from Item c where c.type=1 group by c.technology, c.retailPrice.currency")
 //	Double minimumRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+	
 	@Query("select t.retailPrice.currency, min(t.retailPrice.amount) from Item t where t.type=0 group by t.retailPrice.currency")
-	Map<String,Double> minimumRetailPriceOfToolGroupedByCurrency();
+	List<Object[]> minimumRetailPriceOfToolGroupedByCurrency();
+//	@Query("select t.retailPrice.currency, min(t.retailPrice.amount) from Item t where t.type=0 group by t.retailPrice.currency")
+//	Map<String,Double> minimumRetailPriceOfToolGroupedByCurrency();
 //  @Query("select min(t.retailPrice.amount) from Item t where t.type=0 group by t.retailPrice.currency")
 //	Double minimumRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+	
 	@Query("select p.status ,min(p.budget) from Patronage p group by p.status")
-	Map<PatronageStatus,Double> minimumBudgetOfPatronagesGroupedByPatronageStatus();
+	List<Object[]> minimumBudgetOfPatronagesGroupedByPatronageStatus();
+//	@Query("select p.status ,min(p.budget) from Patronage p group by p.status")
+//	Map<PatronageStatus,Double> minimumBudgetOfPatronagesGroupedByPatronageStatus();
 //  @Query("select min(p.budget) from Patronage p group by p.status")
 //	Double minimumBudgetOfPatronagesGroupedByPatronageStatus();
 	
+	
+	
 //	Maximum 
+	
 	@Query("select c.technology,c.retailPrice.currency, max(c.retailPrice.amount) from Item c where c.type=1 group by c.technology,c.retailPrice.currency")
-	Map<Pair<String,String>,Double> maximumRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+	List<Object[]> maximumRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+//	@Query("select c.technology,c.retailPrice.currency, max(c.retailPrice.amount) from Item c where c.type=1 group by c.technology,c.retailPrice.currency")
+//	Map<Pair<String,String>,Double> maximumRetailPriceOfComponentGroupedByTechnologyAndCurrency();
 //  @Query("select max(c.retailPrice.amount) from Item c where c.type=1 group by c.technology, c.retailPrice.currency")
 //	Double maximumRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+	
+	
 	@Query("select t.retailPrice.currency, max(t.retailPrice.amount) from Item t where t.type=0 group by t.retailPrice.currency")
-	Map<String,Double> maximumRetailPriceOfToolGroupedByCurrency();
+	List<Object[]> maximumRetailPriceOfToolGroupedByCurrency();
+//	@Query("select t.retailPrice.currency, max(t.retailPrice.amount) from Item t where t.type=0 group by t.retailPrice.currency")
+//	Map<String,Double> maximumRetailPriceOfToolGroupedByCurrency();
 //  @Query("select max(t.retailPrice.amount) from Item t where t.type=0 group by t.retailPrice.currency")
 //	Double maximumRetailPriceOfComponentGroupedByTechnologyAndCurrency();
+	
+	
 	@Query("select p.status ,max(p.budget) from Patronage p group by p.status")
-	Map<PatronageStatus,Double> maximumBudgetOfPatronagesGroupedByPatronageStatus();
+	List<Object[]> maximumBudgetOfPatronagesGroupedByPatronageStatus();
+	
+//	@Query("select p.status ,max(p.budget) from Patronage p group by p.status")
+//	Map<PatronageStatus,Double> maximumBudgetOfPatronagesGroupedByPatronageStatus();
 //  @Query("select max(p.budget) from Patronage p group by p.status")
 //	Double maximumBudgetOfPatronagesGroupedByPatronageStatus();
 }
--- src/main/java/acme/features/administrator/dashboard/AdministratorDashboardShowService.java
+++ src/main/java/acme/features/administrator/dashboard/AdministratorDashboardShowService.java
@@ -1,6 +1,7 @@
 package acme.features.administrator.dashboard;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,23 +52,44 @@ public class AdministratorDashboardShowService implements AbstractShowService<Ad
 		final int totalNumberOfComponents;
 		final int totalNumberOfTools;
 		final int totalNumberOfPatronages;
+//		final List<Object[]> totalNumberOfPatronagesGroupedByPatronageStatus;
 		final Map<PatronageStatus,Integer> totalNumberOfPatronagesGroupedByPatronageStatus;
 		
 //		average
+/*
+ * final List<Object[]> averageRetailPriceOfComponentGroupedByTechnologyAndCurrency;
+ * final List<Object[]> averageRetailPriceOfToolGroupedByCurrency;
+ * final List<Object[]> averageBudgetOfPatronagesGroupedByPatronageStatus;
+ */
 		final Map<Pair<String,String>,Double> averageRetailPriceOfComponentGroupedByTechnologyAndCurrency;
 		final Map<String,Double> averageRetailPriceOfToolGroupedByCurrency;
 		final Map<PatronageStatus,Double> averageBudgetOfPatronagesGroupedByPatronageStatus;
 		
 //		deviation
+/*
+ * final List<Object[]> deviationRetailPriceOfComponentGroupedByTechnologyAndCurrency;
+ * final List<Object[]> deviationRetailPriceOfToolGroupedByCurrency;
+ * final List<Object[]> deviationBudgetOfPatronagesGroupedByPatronageStatus;
+ */
 		final Map<Pair<String,String>,Double> deviationRetailPriceOfComponentGroupedByTechnologyAndCurrency;
 		final Map<String,Double> deviationRetailPriceOfToolGroupedByCurrency;
 		final Map<PatronageStatus,Double> deviationBudgetOfPatronagesGroupedByPatronageStatus;
 		
 //		minimum, and maximum 
+/*
+ * final List<Object[]> minimumRetailPriceOfComponentGroupedByTechnologyAndCurrency;
+ * final List<Object[]> minimumRetailPriceOfToolGroupedByCurrency;
+ * final List<Object[]> minimumBudgetOfPatronagesGroupedByPatronageStatus;
+ */
 		final Map<Pair<String,String>,Double> minimumRetailPriceOfComponentGroupedByTechnologyAndCurrency;
 		final Map<String,Double> minimumRetailPriceOfToolGroupedByCurrency;
 		final Map<PatronageStatus,Double> minimumBudgetOfPatronagesGroupedByPatronageStatus;
 		
+		/*
+		 * final List<Object[]> maximumRetailPriceOfComponentGroupedByTechnologyAndCurrency;
+		 * final List<Object[]> maximumRetailPriceOfToolGroupedByCurrency;
+		 * final List<Object[]> maximumBudgetOfPatronagesGroupedByPatronageStatus;
+		 */
 		final Map<Pair<String,String>,Double> maximumRetailPriceOfComponentGroupedByTechnologyAndCurrency;
 		final Map<String,Double> maximumRetailPriceOfToolGroupedByCurrency;
 		final Map<PatronageStatus,Double> maximumBudgetOfPatronagesGroupedByPatronageStatus;
@@ -76,28 +98,43 @@ public class AdministratorDashboardShowService implements AbstractShowService<Ad
 		totalNumberOfComponents=this.repository.totalNumberOfComponents();
 		totalNumberOfTools=this.repository.totalNumberOfTools();
 		totalNumberOfPatronages = this.repository.totalNumberOfPatronages();
-		totalNumberOfPatronagesGroupedByPatronageStatus=this.repository.totalNumberOfPatronagesGroupedByPatronageStatus();
+		totalNumberOfPatronagesGroupedByPatronageStatus=this.repository.totalNumberOfPatronagesGroupedByPatronageStatus().stream()
+			.collect(Collectors.toMap(x-> (PatronageStatus)x[0],x->((Long)x[1]).intValue()));
 		
 //		Average 
 
-		averageRetailPriceOfComponentGroupedByTechnologyAndCurrency=this.repository.averageRetailPriceOfComponentGroupedByTechnologyAndCurrency();
-		averageRetailPriceOfToolGroupedByCurrency=this.repository.averageRetailPriceOfToolGroupedByCurrency();
-		averageBudgetOfPatronagesGroupedByPatronageStatus = this.repository.averageBudgetOfPatronagesGroupedByPatronageStatus();
+		averageRetailPriceOfComponentGroupedByTechnologyAndCurrency=this.repository.averageRetailPriceOfComponentGroupedByTechnologyAndCurrency().stream()
+			.collect(Collectors.toMap(x->Pair.of((String)x[0], (String)x[1]), x->(Double) x[2]));
+		averageRetailPriceOfToolGroupedByCurrency=this.repository.averageRetailPriceOfToolGroupedByCurrency().stream()
+			.collect(Collectors.toMap(x->(String)x[0], x->(Double) x[1]));
+		averageBudgetOfPatronagesGroupedByPatronageStatus = this.repository.averageBudgetOfPatronagesGroupedByPatronageStatus().stream()
+			.collect(Collectors.toMap(x->(PatronageStatus)x[0], x->(Double) x[1]));
 		
 //		Deviation
-		deviationRetailPriceOfComponentGroupedByTechnologyAndCurrency=this.repository.deviationRetailPriceOfComponentGroupedByTechnologyAndCurrency();
-		deviationRetailPriceOfToolGroupedByCurrency=this.repository.deviationRetailPriceOfToolGroupedByCurrency();
-		deviationBudgetOfPatronagesGroupedByPatronageStatus = this.repository.deviationBudgetOfPatronagesGroupedByPatronageStatus();
+		deviationRetailPriceOfComponentGroupedByTechnologyAndCurrency=this.repository.deviationRetailPriceOfComponentGroupedByTechnologyAndCurrency().stream()
+			.collect(Collectors.toMap(x->Pair.of((String)x[0], (String)x[1]), x->(Double) x[2]));
+		deviationRetailPriceOfToolGroupedByCurrency=this.repository.deviationRetailPriceOfToolGroupedByCurrency().stream()
+			.collect(Collectors.toMap(x->(String)x[0], x->(Double) x[1]));
+		deviationBudgetOfPatronagesGroupedByPatronageStatus = this.repository.deviationBudgetOfPatronagesGroupedByPatronageStatus().stream()
+			.collect(Collectors.toMap(x->(PatronageStatus)x[0], x->(Double) x[1]));
 		
 //		Minimum 
-		minimumRetailPriceOfComponentGroupedByTechnologyAndCurrency=this.repository.minimumRetailPriceOfComponentGroupedByTechnologyAndCurrency();
-	    minimumRetailPriceOfToolGroupedByCurrency=this.repository.minimumRetailPriceOfToolGroupedByCurrency();
-	    minimumBudgetOfPatronagesGroupedByPatronageStatus = this.repository.minimumBudgetOfPatronagesGroupedByPatronageStatus();
+		minimumRetailPriceOfComponentGroupedByTechnologyAndCurrency=this.repository.minimumRetailPriceOfComponentGroupedByTechnologyAndCurrency().stream()
+			.collect(Collectors.toMap(x->Pair.of((String)x[0], (String)x[1]), x->(Double) x[2]));
+	    minimumRetailPriceOfToolGroupedByCurrency=this.repository.minimumRetailPriceOfToolGroupedByCurrency().stream()
+			.collect(Collectors.toMap(x->(String)x[0], x->(Double) x[1]));
+	    minimumBudgetOfPatronagesGroupedByPatronageStatus = this.repository.minimumBudgetOfPatronagesGroupedByPatronageStatus().stream()
+			.collect(Collectors.toMap(x->(PatronageStatus)x[0], x->Double.valueOf(x[1].toString())));
 		
 //		Maximum 
-	    maximumRetailPriceOfComponentGroupedByTechnologyAndCurrency=this.repository.maximumRetailPriceOfComponentGroupedByTechnologyAndCurrency();
-		maximumRetailPriceOfToolGroupedByCurrency=this.repository.maximumRetailPriceOfToolGroupedByCurrency();
-		maximumBudgetOfPatronagesGroupedByPatronageStatus = this.repository.maximumBudgetOfPatronagesGroupedByPatronageStatus();
+	    maximumRetailPriceOfComponentGroupedByTechnologyAndCurrency=this.repository.maximumRetailPriceOfComponentGroupedByTechnologyAndCurrency()
+	    	.stream()
+			.collect(Collectors.toMap(x->Pair.of((String)x[0], (String)x[1]), x->(Double) x[2]));
+		maximumRetailPriceOfToolGroupedByCurrency=this.repository.maximumRetailPriceOfToolGroupedByCurrency().stream()
+			.collect(Collectors.toMap(x->(String)x[0], x->(Double) x[1]));
+		maximumBudgetOfPatronagesGroupedByPatronageStatus = this.repository.maximumBudgetOfPatronagesGroupedByPatronageStatus().stream()
+			.collect(Collectors.toMap(x->(PatronageStatus)x[0], x->Double.valueOf(x[1].toString())));
+		
 		
 		result.setTotalNumberOfComponents(totalNumberOfComponents);
 		result.setTotalNumberOfTools(totalNumberOfTools);
