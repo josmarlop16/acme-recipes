86d270efa5f00225bda0bfa3206b55010528daed
Josmarlop16 <josmarlop16@alum.us.es>
2022-03-29 13:12:22 +0200

    Task-048: Done

--- src/main/java/acme/entities/patronages/PatronageReport.java
+++ src/main/java/acme/entities/patronages/PatronageReport.java
@@ -1,7 +1,6 @@
 package acme.entities.patronages;
 
 import java.util.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
@@ -11,10 +10,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
-
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
-
 import acme.framework.entities.AbstractEntity;
 import acme.roles.Inventor;
 import lombok.Getter;
