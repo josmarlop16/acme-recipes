50863a68cb3cb17c3c1be9807e9dbd1ba84ded68
jonatep <jonatep8@gmail.com>
2022-04-22 17:33:23 +0200

    Fix: Arreglado error en test y csv

--- src/test/java/acme/testing/inventor/patronage/InventorPatronageListMineTest.java
+++ src/test/java/acme/testing/inventor/patronage/InventorPatronageListMineTest.java
@@ -14,22 +14,23 @@ public class InventorPatronageListMineTest extends TestHarness {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/inventor/patronage/list-mine.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final String creationMoment, final String title, final String author, final String body, final String emailAddress) {
+	public void positiveTest(final String status, final String code, final String stuff, final String budget, final String periodOfTime, final String optionalLink) {
 		super.signIn("administrator", "administrator");
 
-		super.clickOnMenu("Chirp", "Chirp List");
+		super.clickOnMenu("Inventor", "Patronage List");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
-		
-		super.checkColumnHasValue(0, 0, creationMoment);
+
+		super.checkColumnHasValue(0, 0, status);
 
 		super.clickOnListingRecord(0);
 		super.checkFormExists();
-		super.checkInputBoxHasValue("creationMoment", creationMoment);
-		super.checkInputBoxHasValue("title", title);
-		super.checkInputBoxHasValue("author", author);
-		super.checkInputBoxHasValue("body", body);
-		super.checkInputBoxHasValue("emailAddress", emailAddress);
+		super.checkInputBoxHasValue("status", status);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("stuff", stuff);
+		super.checkInputBoxHasValue("budget", budget);
+		super.checkInputBoxHasValue("periodOfTime", periodOfTime);
+		super.checkInputBoxHasValue("optionalLink", optionalLink);
 
 		super.signOut();
 	}
