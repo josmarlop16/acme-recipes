8c83b0576b80b4910c8e829fe93a3dc3a43c6430
jvegax <toledo.utrera@hotmail.com>
2022-05-17 23:37:38 +0200

    Test became inventor/patron

---
+++ src/test/java/acme/testing/userAccount/BecamePatronTest.java
@@ -0,0 +1,26 @@
+package acme.testing.userAccount;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import acme.testing.TestHarness;
+
+public class BecamePatronTest extends TestHarness {
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/sign-up/positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)
+	public void becameInventor(final String username, final String password, final String name, final String surname, final String email, final String phone) {
+		super.signUp(username, password, name, surname, email);
+		super.signIn(username, password);
+		
+		super.clickOnMenu("Account", "Become a patron");
+		super.fillInputBoxIn("company", "Bulet.io");
+		super.fillInputBoxIn("statement", "lorem");
+		
+		super.clickOnSubmit("Register");
+		
+		super.checkNotErrorsExist();
+	}
+}
